ba38e0d97bcab9495ef608ea647051953cb0ea0f
Tom Oliver Chua <Tom.Chua@ibm.com>
2025-09-07 21:58:42 +0800

    add multidex lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -117,7 +117,8 @@ dependencies {
     implementation("com.google.dagger:hilt-android:$hiltVersion")
     ksp("com.google.dagger:hilt-compiler:$hiltVersion")
     ksp("androidx.hilt:hilt-compiler:$hiltCompilerVersion")
-
+    //Multidex
+    implementation("androidx.multidex:multidex:${multidexVersion}")
 
 
 }
